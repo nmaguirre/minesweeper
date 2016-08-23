054d85b28c480385b8cd97047c1aec742c3cf94b
lbuttignol <leandrobuttignol@gmail.com>
2016-08-23 20:58:07 -0300

    Implementation of the method putMine

--- src.main.java/minesweeper/MinesweeperCell.java
+++ src.main.java/minesweeper/MinesweeperCell.java
@@ -2,12 +2,19 @@ package minesweeper;
 
 public class MinesweeperCell {
 	private boolean blockedCell;
+	private boolean emptyCell;
 
 	public MinesweeperCell (){
 		this.blockedCell = false;
+		this.emptyCell = true;
 	}
 	
 	public boolean isBlocked(){
 		return this.blockedCell;
+	}
+	
+	public void putMine(){
+		if (!this.emptyCell && this.blockedCell ) throw IllegalStateException("Can't put mine while the cell is bloked or is not empty");
+		this.emptyCell = false;
 	}
 }
