75b04908ef18f41a9f2043879000a8768f76c38a
Mariano Ontivero <orevitnoonairam@yahoo.com.ar>
2016-08-23 20:54:17 -0300

    MinesweeperCell.isOpen implementada

--- src.main.java/minesweeper/MinesweeperCell.java
+++ src.main.java/minesweeper/MinesweeperCell.java
@@ -2,12 +2,19 @@ package minesweeper;
 
 public class MinesweeperCell {
 	private boolean blockedCell;
+	private boolean openCell;
 
 	public MinesweeperCell (){
 		this.blockedCell = false;
+		this.openCell = false;
 	}
 	
 	public boolean isBlocked(){
 		return this.blockedCell;
 	}
+	
+	public boolean isOpen(){
+		return this.openCell;
+	}
+	
 }
