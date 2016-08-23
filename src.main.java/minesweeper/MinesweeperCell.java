cc075dd943dd5ed43024ddda7deed10232326b25
gamassimino <gamassimino01@gmail.com>
2016-08-23 20:52:04 -0300

    add test for MinesweeperCell.isBlocked

--- src.main.java/minesweeper/MinesweeperCell.java
+++ src.main.java/minesweeper/MinesweeperCell.java
@@ -1,5 +1,13 @@
 package minesweeper;
 
 public class MinesweeperCell {
+	private boolean blockedCell;
 
+	public MinesweeperCell (){
+		this.blockedCell = false;
+	}
+	
+	public boolean isBlocked(){
+		return this.blockedCell;
+	}
 }
--- src.test.java/minesweeper/MinesweeperCellTest.java
+++ src.test.java/minesweeper/MinesweeperCellTest.java
@@ -1,5 +1,5 @@
 package minesweeper;
-
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -7,8 +7,38 @@ import org.junit.Test;
 public class MinesweeperCellTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void isBlockedTest() {
+	  MinesweeperCell cell = new MinesweeperCell();
+	  cell.putMine();
+	  assertTrue(cell.isBlocked());
+	}
+	
+	@Test
+  public void notBlockedTest() {
+	  MinesweeperCell cell = new MinesweeperCell();	  
+	  assertFalse(cell.isBlocked());
+  }
+
+	public void hasMineFalse(){
+		MinesweeperCell cell = new MinesweeperCell();
+		assertFalse(cell.hasMine());
+	}
+	
+	@Test
+	private void hasMineTrue() {
+		MinesweeperCell cell = new MinesweeperCell();
+		cell.putMine();
+		assertTrue(cell.hasMine());
+	}
+		
+	public void isOpenCell() {
+		MinesweeperCell cell = new MinesweeperCell();
+		assertTrue(cell.isOpen());
+	}
+	@Test
+	public void isClose(){
+		MinesweeperCell cell = new MinesweeperCell();
+		assertTrue(cell.isClose());
 	}
 
 }
