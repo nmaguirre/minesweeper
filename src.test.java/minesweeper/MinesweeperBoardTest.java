e9b9becd6205b1dbf28f39b968d27018fdad920b
fmollea <fmollea@gmail.com>
2016-08-25 19:56:08 -0300

    create MinesweeperBoardTest and MinesweeperGameSateTest classes

---
+++ src.test.java/minesweeper/MinesweeperBoardTest.java
@@ -0,0 +1,14 @@
+package minesweeper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MinesweeperBoardTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
