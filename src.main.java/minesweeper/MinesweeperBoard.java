b20d1bd3abed8a2b33890ed19f6ca1c346089d6e
Nazareno Aguirre <naguirre@gmail.com>
2016-08-25 19:34:38 -0300

    Template for MinesweeperGameState. Folder for Gherkin requirements.

--- src.main.java/minesweeper/MinesweeperBoard.java
+++ src.main.java/minesweeper/MinesweeperBoard.java
@@ -2,4 +2,14 @@ package minesweeper;
 
 public class MinesweeperBoard {
 
+	private MinesweeperCell[][] board;
+	
+	public MinesweeperBoard(int rows, int cols, int mines) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int numberOfMinedNeighbours(int row, int col) {
+		return 0;
+	}
+
 }
