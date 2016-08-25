029844af04a4b08dd3c6c5436c296bf0ddb8808e
gamassimino <gamassimino01@gmail.com>
2016-08-25 19:56:31 -0300

    MinesweeperBoard.MinesweeperBoard is done

--- src.main.java/minesweeper/MinesweeperBoard.java
+++ src.main.java/minesweeper/MinesweeperBoard.java
@@ -3,9 +3,15 @@ package minesweeper;
 public class MinesweeperBoard {
 
 	private MinesweeperCell[][] board;
+	private int boardRows;
+	private int boardCols;
+	private int boardMines;
 	
 	public MinesweeperBoard(int rows, int cols, int mines) {
 		// TODO Auto-generated constructor stub
+	  boardRows = rows;
+	  boardCols = cols;
+	  boardMines = mines;
 	}
 
 	public int numberOfMinedNeighbours(int row, int col) {
