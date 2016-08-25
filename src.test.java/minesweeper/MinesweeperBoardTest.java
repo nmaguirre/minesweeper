b7953c98e175882a6136f9dc4b456478b121800e
Ezequiel Depetris <ezedepetris@gmail.com>
2016-08-25 20:11:19 -0300

    test for the constructor MinesweeperBoard

--- src.test.java/minesweeper/MinesweeperBoardTest.java
+++ src.test.java/minesweeper/MinesweeperBoardTest.java
@@ -7,8 +7,12 @@ import org.junit.Test;
 public class MinesweeperBoardTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void ConstructorTest() {
+    MinesweeperBoard board = new MinesweeperBoard(5,5,10);
+    boolean checkRows = board.getBoardRows() == 5;
+    boolean checkCols = board.getBoardCols() == 5;
+    boolean checkMines = board.getBoardMines() == 10;
+    assertTrue(checkRows && checkCols && checkMines);
 	}
 
 }
