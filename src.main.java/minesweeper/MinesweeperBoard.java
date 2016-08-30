47579ae4648f099cc97b8bcb38fd6035d9123ad9
Diana Gonzalez <dianagonzalez22.dg@gmail.com>
2016-08-30 18:28:25 -0300

    [#66] Resolved.

--- src.main.java/minesweeper/MinesweeperBoard.java
+++ src.main.java/minesweeper/MinesweeperBoard.java
@@ -142,6 +142,17 @@ public class MinesweeperBoard {
         MinesweeperCell cell = board[row][col];
         return !cell.isClose();
     }
+    
+    /**
+     * @param row file number of the board where the cell is located.
+     * @param col column number of the board where the cell is located.
+     * This method open a cell in a position.
+     */
+    public void open(int row, int col) {
+    	MinesweeperCell cell = board[row][col];
+        cell.open();
+    }
+
 
     public void mark(int row, int col) {
     	if (isValidIndex(row,col)){
