96f7bb10d4c9f2dc95212e0c8391786018458a8f
fernando13 <fernandobentolila_13@hotmail.com>
2016-08-30 18:24:37 -0300

    MinesBoard.isOpened and putMine has been tested

--- src.test.java/minesweeper/MinesweeperBoardTest.java
+++ src.test.java/minesweeper/MinesweeperBoardTest.java
@@ -13,5 +13,44 @@ public class MinesweeperBoardTest {
         boolean checkMines = board.getBoardMines() == 10;
         assertTrue(checkRows && checkCols && checkMines);
     }
-
+    
+    @Test
+	public void isOpenedTrueTest(){
+    	MinesweeperBoard board = new MinesweeperBoard(5,5,0);
+    	board.open(1,2);
+        assertTrue(board.isOpened(1, 2));
+	}
+    
+    @Test
+	public void isOpenedFalseTest(){
+    	MinesweeperBoard board = new MinesweeperBoard(5,5,0);
+        assertFalse(board.isOpened(1,2));
+	}
+    
+    @Test
+    private void putMineTrueTest() {
+    	MinesweeperBoard board = new MinesweeperBoard(5,5,0);
+    	board.putMine(1, 2);
+    	assertTrue(board.hasMine(1,2));
+	}
+    
+    @Test
+    private void putMineFalseTest() {
+    	MinesweeperBoard board = new MinesweeperBoard(5,5,0);
+    	assertFalse(board.hasMine(2,2));
+	}
+    
+    @Test(expected=IllegalStateException.class)
+    private void putMineExeptionTest() {
+    	MinesweeperBoard board = new MinesweeperBoard(5,5,0);
+    	board.putMine(2, 2);
+    	board.putMine(2, 2);
+	}
+    
+    @Test(expected=IllegalStateException.class)
+    private void putMineExeptionTest2() {
+    	MinesweeperBoard board = new MinesweeperBoard(5,5,0);
+    	board.open(2, 2);
+    	board.putMine(2, 2);
+    }	
 }
