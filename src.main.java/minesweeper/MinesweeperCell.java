3ebca6da625c0be63ecad0b744bcd9ee93d36a39
jortolano <ortolano33@gmail.com>
2016-09-01 11:59:44 -0300

    name change to the fields of the class according to convention ISX minesweeper.cell

--- src.main.java/minesweeper/MinesweeperCell.java
+++ src.main.java/minesweeper/MinesweeperCell.java
@@ -10,41 +10,41 @@ public class MinesweeperCell {
      * hasMine represents if the cell has a mine inside or not
      * openCell represents if the cell is open or not
      */
-	private boolean blockedCell;
-    private boolean hasMine;
-    private boolean openCell;
+	private boolean isBlockedCell;
+    private boolean isMine;
+    private boolean isOpenCell;
 
     public MinesweeperCell() {
-        this.blockedCell = false;
-        this.hasMine = false;
-        this.openCell = false;
+        this.isBlockedCell = false;
+        this.isMine = false;
+        this.isOpenCell = false;
     }
 
     /**
      * @return true if cell is blocked
      */
     public boolean isBlocked() {
-        return this.blockedCell;
+        return this.isBlockedCell;
     }
 
     /**
      * @return true if the cell is close
      */
     public boolean isClose() {
-        return !(this.openCell);
+        return !(this.isOpenCell);
     }
 
     /**
      * This method fill the cell with a mine.
      */
     public void putMine() {
-        if (this.hasMine || this.openCell)
+        if (this.isMine || this.isOpenCell)
             throw new IllegalStateException("Can't put mine while the cell is open or already have a mine ");
-        this.hasMine = true;
+        this.isMine = true;
     }
 
     public boolean isOpen() {
-        return this.openCell;
+        return this.isOpenCell;
     }
 
     /**
@@ -53,7 +53,7 @@ public class MinesweeperCell {
      */
     public void open() {
         if (this.isClose()) {
-            this.openCell = true;
+            this.isOpenCell = true;
         }
     }
 
@@ -61,7 +61,7 @@ public class MinesweeperCell {
      * @return true iff cell has a mine
      */
     public boolean hasMine() {
-        return this.hasMine;
+        return this.isMine;
     }
 
     /**
@@ -70,7 +70,7 @@ public class MinesweeperCell {
     public void block() {
         if (this.isOpen())
             throw new IllegalStateException("Can't block a cell when it's open ");
-        this.blockedCell = true;
+        this.isBlockedCell = true;
     }
 
     /**
@@ -79,6 +79,6 @@ public class MinesweeperCell {
      */
     public void unblock() {
     	if (!this.isBlocked()) throw new IllegalStateException("Can't unblock an unblocked or opened cell");
-        this.blockedCell = false;
+        this.isBlockedCell = false;
     }
 }
