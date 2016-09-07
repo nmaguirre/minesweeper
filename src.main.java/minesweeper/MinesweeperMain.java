ef9e01f97850cb311ce13b5b42b596f8bc164377
Guttlein <emilianog09@gmail.com>
2016-09-07 14:51:26 -0300

    Don some changes on getUserInput(). Hope it´s solved now.

--- src.main.java/minesweeper/MinesweeperMain.java
+++ src.main.java/minesweeper/MinesweeperMain.java
@@ -1,4 +1,7 @@
 package minesweeper;
+
+import java.util.Scanner;
+
 /** 
  * Main class of the minesweeper game, CLI version.
  * TODO improve this description
@@ -10,20 +13,37 @@ public class MinesweeperMain {
 
 	private static MinesweeperGameState game;
 	
+	private static Scanner comScanner;
+	private static Scanner coord1Scanner;
+	private static Scanner coord2Scanner;
+	
 	
 	/**
 	 * Take the actions provided by the user and return a String with the action and cell.
 	 * @return a String with the action and cell.
 	 */
-	public static String getUserInput(){
-		String command;
-		String coord1;
-		String coord2;
-		command = System.in.toString();
-		coord1 = System.in.toString();
-		coord2 = System.in.toString();
-		return command + coord1 + "," + coord2;
-	}
+
+
+		public static String getUserInput(){
+			String command;
+			String coord1= new String("");
+			String coord2= new String("");
+			comScanner = new Scanner(System.in);
+			coord1Scanner = new Scanner(System.in);
+			coord2Scanner = new Scanner(System.in);
+			
+			command = comScanner.nextLine();
+			try {
+				if(command!="o" || command!="b" || command!="u" || command!="O" || command!="B" || command!="U"){
+					System.out.println("Comando incorrecto");
+					getUserInput();
+				}
+			}catch(Exception e){
+				coord1 = coord1Scanner.nextLine();
+				coord2 = coord2Scanner.nextLine();
+			}
+			return command + coord1 + "," + coord2;
+		}
 	
 	/**
 	 * Runs minesweeper game with simple settings:
