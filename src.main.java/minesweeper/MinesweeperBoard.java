8fa687919250627fbc8b617bf5bb728a9cc9caea
Elisa Boselli <elisaboselli@hotmail.com>
2016-09-08 20:06:50 -0300

    The best thing ever

--- src.main.java/minesweeper/MinesweeperBoard.java
+++ src.main.java/minesweeper/MinesweeperBoard.java
@@ -334,7 +334,10 @@ public class MinesweeperBoard {
     public String toString() {
     	String result = "";
     	MinesweeperCell actualCell;
+    	result = result + ("   0  1  2  3  4  5  6  7  8  9 \n");
+    	result = result + ("   ---------------------------- \n");
     	for (int row = 0; row<this.getRowCount(); row++){
+    		result = result + row + "|";
     		for (int col = 0; col<this.getColCount(); col++){
     			actualCell = board[row][col];
     			if(actualCell.isOpen()){
@@ -358,8 +361,10 @@ public class MinesweeperBoard {
     				}
     			}
     		}
-    		result = result + "\n";
+    		result = result + "|" + row + "\n";
     	}
+    	result = result + ("   ---------------------------- \n");
+    	result = result + ("   0  1  2  3  4  5  6  7  8  9\n");
     	return result;
     }
 
